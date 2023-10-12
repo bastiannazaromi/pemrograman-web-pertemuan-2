8b08f49e960cee185f8bc51ec15e5096bef03cab
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2023-10-12 08:19:05 +0700

    edit judul

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Halam Login</title>
+	<title>Halaman Login</title>
 
 	<link rel="stylesheet" type="text/css" href="src/style.css">
 </head>
